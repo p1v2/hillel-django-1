69a9791449ae598d7d45e47f2bd6eebe540c82c1
m1rageg <egor.sergei.demin153@gmail.com>
2023-02-24 02:05:47 +0200

    create new field and change country field

--- books/models/book.py
+++ books/models/book.py
@@ -9,10 +9,11 @@ class Book(models.Model):
     name = models.CharField(max_length=40, unique=True)
     pages_count = models.IntegerField(null=True)
     authors = models.ManyToManyField(Author)
-    country = models.ForeignKey(Country, null=True, on_delete=models.CASCADE)
+    country = models.ForeignKey(Country, null=True, on_delete=models.CASCADE,blank=True)
     price = models.FloatField()
     seller = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
     count_sold = models.IntegerField(default=0)
+    is_archived = models.BooleanField(null=True)
 
     @property
     def authors_string(self):
