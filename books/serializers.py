155010a9486f402925d3b36796616f38946f1d14
m1rageg <egor.sergei.demin153@gmail.com>
2023-02-24 02:06:32 +0200

    change country and authors fields

--- books/serializers.py
+++ books/serializers.py
@@ -21,8 +21,8 @@ class AuthorSerializer(serializers.ModelSerializer):
 
 
 class BookSerializer(serializers.ModelSerializer):
-    country = CountrySerializer()
-    authors = AuthorSerializer(many=True)
+    country = CountrySerializer(read_only=True)
+    authors = AuthorSerializer(many=True, read_only=True)
 
     serializer_method_field = serializers.SerializerMethodField("calculate_field")
 
@@ -32,7 +32,7 @@ class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = ("id", "name", "country", "authors", "seller", "authors_string", "serializer_method_field",
-                  "count_sold")
+                  "count_sold", "price")
 
 
 class OrderLineItemSerializer(serializers.ModelSerializer):
