e6dcdeef810b934bec6189c200c3c1098fb8c8d4
m1rageg <egor.sergei.demin153@gmail.com>
2023-03-18 23:48:54 +0200

    fix

--- books/telegram.py
+++ books/telegram.py
@@ -2,18 +2,16 @@ import os
 
 import requests
 
+
 TELEGRAM_TOKEN = os.environ["TELEGRAM_TOKEN"]
 TELEGRAM_USER_ID = os.environ["TELEGRAM_USER_ID"]
 
 
-def send_telegram_message(text):
+def send_telegram_message(text, telegram_account_id='TELEGRAM_USER_ID'):
     url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
 
     body = {
-        "chat_id": TELEGRAM_USER_ID,
+        "chat_id": telegram_account_id,
         "text": text,
     }
-    response = requests.post(url, json=body)
-    print(response.text)
-    response.raise_for_status()
-
+    requests.post(url, json=body)
