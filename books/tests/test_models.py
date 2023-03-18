8982d73dc633418a0535f8d6aeec35a8337ef27a
m1rageg <egor.sergei.demin153@gmail.com>
2023-03-18 23:51:07 +0200

    add some tests

--- books/tests/test_models.py
+++ books/tests/test_models.py
@@ -1,8 +1,11 @@
+import random
+
 from django.test import TestCase
 
 from faker import Faker
 
-from books.models import Book, Author
+from books.models import Book, Author, Country
+from django.contrib.auth.models import User
 
 
 class BookTestCase(TestCase):
@@ -12,6 +15,9 @@ class BookTestCase(TestCase):
 
         self.author1 = Author.objects.create(first_name=fake.first_name(), last_name=fake.last_name())
         self.author2 = Author.objects.create(first_name=fake.first_name(), last_name=fake.last_name())
+
+        self.country = Country.objects.create(name=random.choice(['Germany', 'Poland', 'Ukraine']))
+        self.user = User.objects.create(username=fake.user_name())
 
     def test_authors_string_zero_authors(self):
         authors_string = self.book.authors_string
@@ -33,3 +39,8 @@ class BookTestCase(TestCase):
         self.assertEqual(authors_string,
                          f"{self.author1.first_name} {self.author1.last_name}, "
                          f"{self.author2.first_name} {self.author2.last_name}")
+
+    def test_get_information(self):
+        book_test_info = self.book.get_information()
+        self.assertEqual(book_test_info,
+                         "Book's name is Cobsar\nCountry is Netherlands\nCount of pages: 228\nPrice is 700")
