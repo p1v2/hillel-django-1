95662b53939aa28c5fc217e0f4e44e895bf94a42
VasylSiromakha <a0661833889@gmail.com>
2023-04-05 21:27:59 +0300

    fix app students

--- books/management/commands/stud_commands.py
+++ books/management/commands/stud_commands.py
@@ -1,8 +1,7 @@
 from django.core.management import BaseCommand
 from students.models import Students, Lessons, Teachers, LessonStudent
 from django.db.models import Count
-from django.db.models import Avg, Max
-
+from django.db.models import Avg, Max, Q
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
@@ -13,7 +12,7 @@ class Command(BaseCommand):
 
         # Усі студенти які отримали оцінку 8 або більше за певну дату.
 
-        lesson_students = LessonStudent.objects.filter(mark__gte=8, lesson__date_time__startswith='2022-11-21')
+        lesson_students = LessonStudent.objects.filter(mark__gte=8, lesson__date_time__date='2022-11-21')
         for lesson_student in lesson_students:
             print(lesson_student.student.first_name, lesson_student.student.last_name, lesson_student.mark,
                   lesson_student.lesson.date_time)
@@ -40,9 +39,10 @@ class Command(BaseCommand):
             .order_by('-avg_mark')
         )
 
-        students = Students.objects.filter(id__in=[s['student'] for s in students_with_higher_avg])
-        for student in students:
-            print(student.first_name, student.last_name)
+        student_ids = students_with_higher_avg.values_list('student', flat=True)
+        students = Students.objects.filter(id__in=student_ids).values_list('first_name', 'last_name')
+        for first_name, last_name in students:
+            print(first_name, last_name)
 
         # Топ-5 студентів із найвищою оцінкою за всі уроки.
 
@@ -53,31 +53,35 @@ class Command(BaseCommand):
             .filter(avg_mark__isnull=False)
             .order_by('-max_mark', '-avg_mark')[:5]
         )
-        students = Students.objects.filter(id__in=[s['student'] for s in top_5_students])
-        for student in students:
-            print(student.first_name, student.last_name)
+        student_ids = top_5_students.values_list('student', flat=True)
+        students = Students.objects.filter(id__in=student_ids).values_list('first_name', 'last_name')
+        for first_name, last_name in students:
+            print(first_name, last_name)
 
         # Відсоток студентів, які отримали оцінку 5 або вище для кожного уроку.
+        queryset = (
+            LessonStudent.objects
+            .values('lesson_id')
+            .annotate(
+                num_students=Count('id'),
+                num_passing_students=Count('id', filter=Q(mark__gte=5)),
+                percent_passing_students=100 * Count('id', filter=Q(mark__gte=5)) / Count('id'),
+            )
+            .order_by('lesson_id')
+            .values('lesson_id', 'percent_passing_students')
+        )
 
-        lessons = LessonStudent.objects.values('lesson_id').distinct()
+        for row in queryset:
+            print(f"Урок {row['lesson_id']}: {row['percent_passing_students']}% студентів отримали оцінку 5 або вище.")
 
-        for lesson in lessons:
-            lesson_id = lesson['lesson_id']
-            students_count = LessonStudent.objects.filter(lesson_id=lesson_id).count()
-            top_students_count = LessonStudent.objects.filter(lesson_id=lesson_id, mark__gte=5).count()
-            if students_count > 0:
-                top_students_percentage = (top_students_count / students_count) * 100
-            else:
-                top_students_percentage = 0
-            print(f"Lesson {lesson_id}: {top_students_percentage:.2f}% students got 5 or higher")
-
-        # Кількість уроків, які провів кожен викладач.
+        # # Кількість уроків, які провів кожен викладач.
         teachers =Teachers.objects.annotate(lesson_count=Count('lessons')).values('first_name', 'last_name', 'lesson_count')
         for teacher in teachers:
             print(f"{teacher['first_name']} {teacher['last_name']}: {teacher['lesson_count']} lessons")
 
-        # Всі уроки, які провів викладач з прізвищем "Rogers" обʼєднані з уроками які провів викладач з прізвищем "Huerta".
+        # # Всі уроки, які провів викладач з прізвищем "Rogers" обʼєднані з уроками які провів викладач з прізвищем "Huerta".
         lessons = Lessons.objects.filter(teacher__last_name__in=['Rogers', 'Huerta'])
         for lesson in lessons:
             print(
                 f"Lesson topic: {lesson.topic}, Teacher: {lesson.teacher.first_name} {lesson.teacher.last_name}")
+
