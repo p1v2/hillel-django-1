13c52f64fa5675fd2a40a73a8982f2892bd1ef3d
Gregory <gregory@gmail.com>
2023-04-30 22:08:45 +0300

    Github authorization

--- hillel_django/settings.py
+++ hillel_django/settings.py
@@ -54,7 +54,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'allauth.socialaccount.providers.google',
-    'allauth.socialaccount.providers.telegram',
+    'allauth.socialaccount.providers.github',
     'celery',
     'books',
     'customers',
@@ -141,7 +141,7 @@ else:
 
 DATABASES = {
     'default': DEFAULT_DATABASE,
-    'student': dj_database_url.parse("postgres://vgzibcntmwkwjt:fa5731545dbdb678b43e21db8ba7e214225d3bc7eec20ebd87f1d2cfce50e636@ec2-34-250-252-161.eu-west-1.compute.amazonaws.com:5432/df6mqa9p7d1v6o")
+    #'student': dj_database_url.parse("postgres://vgzibcntmwkwjt:fa5731545dbdb678b43e21db8ba7e214225d3bc7eec20ebd87f1d2cfce50e636@ec2-34-250-252-161.eu-west-1.compute.amazonaws.com:5432/df6mqa9p7d1v6o")
 }
 
 # Password validation
@@ -318,9 +318,10 @@ SOCIALACCOUNT_PROVIDERS = {
             'https://www.googleapis.com/auth/drive',
         ],
         'AUTH_PARAMS': {
-            'access_type': 'offline',
+            'access_type': 'online',
         }
     },
+    "github": {}
 }
 
 # Django-allauth
